1dadaf2c963f19ec331f1a3a0717bf3d4b744287
Nathandevanw <thaivanw@gmail.com>
2025-08-14 12:39:52 +1000

    123

--- Question3_process.php
+++ Question3_process.php
@@ -9,13 +9,11 @@ if ($_POST['email'] === $valid_email && $_POST['password'] === $valid_password) 
     $_SESSION['verification_code'] = $code;
     $_SESSION['code_expiry'] = time() + 300; // 5 minutes validity
 
-    $to = $valid_email;
     $subject = "Your 2FA Verification Code";
     $message = "Your verification code is: $code";
     $headers = "From: no-reply@ecommerce.local";
 
-    // Send the email (ensure local mail server is configured)
-    mail($to, $subject, $message, $headers);
+    mail($valid_email, $subject, $message, $headers);
 
     echo "<p>We have sent a secret code to your email.</p>";
     echo '<form action="Question3_Authenticate.php" method="post">
